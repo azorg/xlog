9558bd6ee4bb0d3c1e6a164275a0463f664655e6
Alex Zorg <azorg@mail.ru>
2024-03-04 14:39:02 +0300

    some fix flag.go

--- flag.go
+++ flag.go
@@ -25,32 +25,32 @@ type Opt struct {
 // Setup command line logger options
 // Usage:
 //
-//	-log <level>      - Log level (flood/trace/debug/info/notice/warm/error/critical)
-//	-slog             - Use structured text logger (slog)
-//	-jlog             - Use structured JSON logger (slog)
-//	-tlog             - Use tinted (colorized) logger (tint)
-//	-lsrc|-lnosrc     - Force on/off log source file name and line number
-//	-lpkg|-lnopkg     - Force on/off log source directory/file name and line number
-//	-ltime|-lnotime   - Force on/off timestamp
-//	-ltimefmt         - Override log time format (e.g. 15:04:05.999 or TimeOnly)
-//	-lnolevel         - Disable log level tag (~level="INFO")
-//	-lcolor|-lnocolor - Force enable/disable tinted colors
+//	-log <level>       - log level (flood/trace/debug/info/notice/warm/error/critical)
+//	-slog              - use structured text logger (slog)
+//	-jlog              - use structured JSON logger (slog)
+//	-tlog              - use tinted (colorized) logger (tint)
+//	-lsrc|-lnosrc      - force on/off log source file name and line number
+//	-lpkg|-lnopkg      - force on/off log source directory/file name and line number
+//	-ltime|-lnotime    - force on/off timestamp
+//	-ltimefmt <format> - override log time format (e.g. 15:04:05.999 or TimeOnly)
+//	-lnolevel          - disable log level tag (~level="INFO")
+//	-lcolor|-lnocolor  - force enable/disable tinted colors
 func NewOpt() *Opt {
 	opt := &Opt{}
-	flag.StringVar(&opt.Level, "log", "", "Override log level (flood/trace/debug/info/warm/error/fatal)")
-	flag.BoolVar(&opt.SLog, "slog", false, "Use structured text logger (slog)")
-	flag.BoolVar(&opt.JLog, "jlog", false, "Use structured JSON logger (slog)")
-	flag.BoolVar(&opt.TLog, "tlog", false, "Use tinted (colorized) logger (tint)")
-	flag.BoolVar(&opt.Src, "lsrc", false, "Force log source file name and line number")
-	flag.BoolVar(&opt.NoSrc, "lnosrc", false, "Force off source file name and line number")
-	flag.BoolVar(&opt.Pkg, "lpkg", false, "Force log source directory/file name and line number")
-	flag.BoolVar(&opt.NoPkg, "lnopkg", false, "Force off source directory/file name and line number")
-	flag.BoolVar(&opt.Time, "ltime", false, "Force add timestamp to log")
-	flag.BoolVar(&opt.NoTime, "lnotime", false, "Force off timestamp")
-	flag.StringVar(&opt.TimeFmt, "ltimefmt", "", "Override log time format (e.g. 15:04:05.999 or TimeOnly)")
-	flag.BoolVar(&opt.NoLevel, "lnolevel", false, `Disable log level tag (~level="INFO")`)
-	flag.BoolVar(&opt.Color, "lcolor", false, "Force enable tinted colors")
-	flag.BoolVar(&opt.NoColor, "lnocolor", false, "Force disable tinted colors")
+	flag.StringVar(&opt.Level, "log", "", "override log level (flood/trace/debug/info/warm/error/fatal)")
+	flag.BoolVar(&opt.SLog, "slog", false, "use structured text logger (slog)")
+	flag.BoolVar(&opt.JLog, "jlog", false, "use structured JSON logger (slog)")
+	flag.BoolVar(&opt.TLog, "tlog", false, "use tinted (colorized) logger (tint)")
+	flag.BoolVar(&opt.Src, "lsrc", false, "force log source file name and line number")
+	flag.BoolVar(&opt.NoSrc, "lnosrc", false, "force off source file name and line number")
+	flag.BoolVar(&opt.Pkg, "lpkg", false, "force log source directory/file name and line number")
+	flag.BoolVar(&opt.NoPkg, "lnopkg", false, "force off source directory/file name and line number")
+	flag.BoolVar(&opt.Time, "ltime", false, "force add timestamp to log")
+	flag.BoolVar(&opt.NoTime, "lnotime", false, "force off timestamp")
+	flag.StringVar(&opt.TimeFmt, "ltimefmt", "", "override log time format (e.g. 15:04:05.999 or TimeOnly)")
+	flag.BoolVar(&opt.NoLevel, "lnolevel", false, `disable log level tag (~level="INFO")`)
+	flag.BoolVar(&opt.Color, "lcolor", false, "force enable tinted colors")
+	flag.BoolVar(&opt.NoColor, "lnocolor", false, "force disable tinted colors")
 	return opt
 }
 
